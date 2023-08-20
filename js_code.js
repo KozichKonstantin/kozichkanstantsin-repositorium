9f0a79b33ec0b41aebf4d2281b6661a64ea84e6e
KozichKonstantin <humanstepyt@gmail.com>
2023-08-20 23:59:56 +0300

    20.08.2023 V1.01.1
    added some hover effects
    tryed to add jquery, but got errors
    /*i'm an idiot*/

--- js_code.js
+++ js_code.js
@@ -1,6 +1,19 @@
-let name = document.getElementById('barbarian');
-function consicFunction(){
-    let name = 'nameOfCard';
-}
-console.log(name)
-//img src = "data/grid_cards/${NAME}/${imageNumber}.jpg"
+// let name = document.getElementById('barbarian');
+// function consicFunction(){
+//     let name = 'nameOfCard';
+// }
+// console.log(name)
+ $(document).ready(function(){
+    $(window).bind("resize", resizeWindow);
+    function resizeWindow(e){
+      var newWindowWidth = $(window).width();
+
+      // Если ширина меньше 600 px, используется таблица стилей для мобильного
+      if(newWindowWidth < 1100){
+        $("link[rel=stylesheet]").attr({href : "css_verst_mobile.css"});
+      } else if(newWindowWidth > 1100){
+        // Если ширина больше 600 px, используется таблица стилей для десктопа
+        $("link[rel=stylesheet]").attr({href : "css_verst.css"});
+      }
+    }
+  });
