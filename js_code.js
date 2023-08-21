e419a4451b597a1a5ddbb9cbbc371475ea3a71dd
KozichKonstantin <humanstepyt@gmail.com>
2023-08-21 15:39:53 +0300

    21.08.2023 V1.01.1
    just got errors with js

--- js_code.js
+++ js_code.js
@@ -3,17 +3,23 @@
 //     let name = 'nameOfCard';
 // }
 // console.log(name)
- $(document).ready(function(){
-    $(window).bind("resize", resizeWindow);
-    function resizeWindow(e){
-      var newWindowWidth = $(window).width();
+//  $(document).ready(function(){
+//     $(window).bind("resize", resizeWindow);
+//     function resizeWindow(e){
+//       var newWindowWidth = $(window).width();
 
-      // Если ширина меньше 600 px, используется таблица стилей для мобильного
-      if(newWindowWidth < 1100){
-        $("link[rel=stylesheet]").attr({href : "css_verst_mobile.css"});
-      } else if(newWindowWidth > 1100){
-        // Если ширина больше 600 px, используется таблица стилей для десктопа
-        $("link[rel=stylesheet]").attr({href : "css_verst.css"});
-      }
-    }
-  });
+//       // Если ширина меньше 600 px, используется таблица стилей для мобильного
+//       if(newWindowWidth < 1100){
+//         $("link[rel=stylesheet]").attr({href : "css_verst_mobile.css"});
+//       } else if(newWindowWidth > 1100){
+//         // Если ширина больше 600 px, используется таблица стилей для десктопа
+//         $("link[rel=stylesheet]").attr({href : "css_verst.css"});
+//       }
+//     }
+//   });
+const slider = document.querySelector('.image barbarian'),
+    leftButton = document.querySelectorAll( '.slideButton_case_left'),
+    rightButton = document.querySelectorAll('.slideButton_case_right')
+leftButton.addEventListener('click', function prevSlide() {
+    document.getElementsByClassName('.image').style.backgroundColor = black;
+})
