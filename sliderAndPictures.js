21c2bb1183764cb8660fe040409343ef26cacf66
KozichKonstantin <humanstepyt@gmail.com>
2023-08-23 14:40:53 +0300

    23.08.2023 V1.01.1
    finished new slider

--- sliderAndPictures.js
+++ sliderAndPictures.js
@@ -77,8 +77,8 @@ const base = [
 let leftButtonsMassiv = document.getElementsByClassName('slideButton_case_left')
 let rightButtonsMassiv = document.getElementsByClassName('slideButton_case_right')
 for(let i=0; i < leftButtonsMassiv.length; i++){
-  var cardFirstImage = document.getElementsByClassName("image " + base[i]['name'])[0]
-  cardFirstImage.style.backgroundImage = 'url' + '(' + base[i]['img'] + '/' + base[i]['currentPicture'] + '.jpg' + ')';
+  var cardFirstImage = document.getElementsByClassName("frame " + base[i]['name'])[0]
+  cardFirstImage.src = base[i]['img'] + '/' + base[i]['currentPicture'] + '.jpg';
   console.log(i)
 }
 console.log(leftButtonsMassiv)
@@ -93,9 +93,9 @@ for(let i=0; i<leftButtonsMassiv.length; i++){
         base[j]['currentPicture'] = base[j]['currentPicture'] -1;
         if(base[j]['currentPicture']>= 1){
             pictureFill = picture.slice(0, -1);
-            pictureFill = 'url' + '(' + pictureFill + base[j]['currentPicture'] + '.jpg' + ')';
-            var slider = document.getElementsByClassName("image " + buttonLeftSrav)[0];
-            slider.style.backgroundImage = pictureFill;
+            pictureFill = pictureFill + base[j]['currentPicture'] + '.jpg' ;
+            var slider = document.getElementsByClassName("frame " + buttonLeftSrav)[0];
+            slider.src = pictureFill;
             console.log(pictureFill);
         }
         else{
@@ -120,9 +120,9 @@ for(let i=0; i<rightButtonsMassiv.length; i++){
         base[j]['currentPicture'] = base[j]['currentPicture'] +1;
         if(base[j]['currentPicture'] < base[j]['count']){
             pictureFill = picture.slice(0, -1);
-            pictureFill = 'url' + '(' + pictureFill + base[j]['currentPicture'] + '.jpg' + ')';
-            var slider = document.getElementsByClassName("image " + buttonRightSrav)[0];
-            slider.style.backgroundImage = pictureFill;
+            pictureFill = pictureFill + base[j]['currentPicture'] + '.jpg' ;
+            var slider = document.getElementsByClassName("frame " + buttonRightSrav)[0];
+            slider.src = pictureFill;
             console.log(pictureFill);
         }
         else{
@@ -137,3 +137,70 @@ for(let i=0; i<rightButtonsMassiv.length; i++){
     }
   })
 }
+
+
+
+
+// let leftButtonsMassiv = document.getElementsByClassName('slideButton_case_left')
+// let rightButtonsMassiv = document.getElementsByClassName('slideButton_case_right')
+// for(let i=0; i < leftButtonsMassiv.length; i++){
+//   var cardFirstImage = document.getElementsByClassName("image " + base[i]['name'])[0]
+//   cardFirstImage.style.backgroundImage = 'url' + '(' + base[i]['img'] + '/' + base[i]['currentPicture'] + '.jpg' + ')';
+//   console.log(i)
+// }
+// console.log(leftButtonsMassiv)
+
+// for(let i=0; i<leftButtonsMassiv.length; i++){
+//   leftButtonsMassiv[i].addEventListener('click', function() {
+//     const clickedLeftButton = leftButtonsMassiv[i];
+//     let buttonLeftSrav = clickedLeftButton.parentNode.className.toString().slice(6, (clickedLeftButton.length));
+//     for(let j=0; j < leftButtonsMassiv.length; j++){
+//       if(buttonLeftSrav == base[j]['name']){
+//         var picture = base[j]['img'] + '/' + base[j]['currentPicture'];
+//         base[j]['currentPicture'] = base[j]['currentPicture'] -1;
+//         if(base[j]['currentPicture']>= 1){
+//             pictureFill = picture.slice(0, -1);
+//             pictureFill = 'url' + '(' + pictureFill + base[j]['currentPicture'] + '.jpg' + ')';
+//             var slider = document.getElementsByClassName("image " + buttonLeftSrav)[0];
+//             slider.style.backgroundImage = pictureFill;
+//             console.log(pictureFill);
+//         }
+//         else{
+//           console.log('errorOutOfRange')
+//           base[j]['currentPicture']++;
+//           console.log(pictureFill)
+//         }
+//       }
+//       else{
+//         console.log('errorNoSuchClass')
+//       }
+//     }
+//   })
+// }
+// for(let i=0; i<rightButtonsMassiv.length; i++){
+//   rightButtonsMassiv[i].addEventListener('click', function() {
+//     const clickedRightButton = rightButtonsMassiv[i];
+//     let buttonRightSrav = clickedRightButton.parentNode.className.toString().slice(6, (clickedRightButton.length));
+//     for(let j=0; j < rightButtonsMassiv.length; j++){
+//       if(buttonRightSrav == base[j]['name']){
+//         var picture = base[j]['img'] + '/' + base[j]['currentPicture'];
+//         base[j]['currentPicture'] = base[j]['currentPicture'] +1;
+//         if(base[j]['currentPicture'] < base[j]['count']){
+//             pictureFill = picture.slice(0, -1);
+//             pictureFill = 'url' + '(' + pictureFill + base[j]['currentPicture'] + '.jpg' + ')';
+//             var slider = document.getElementsByClassName("image " + buttonRightSrav)[0];
+//             slider.style.backgroundImage = pictureFill;
+//             console.log(pictureFill);
+//         }
+//         else{
+//           console.log('errorOutOfRange')
+//           base[j]['currentPicture']--;
+//           console.log(pictureFill)
+//         }
+//       }
+//       else{
+//         console.log('errorNoSuchClass')
+//       }
+//     }
+//   })
+// }
