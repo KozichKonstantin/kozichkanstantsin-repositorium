e6842b71ad2aa04d2e98cfb38bc8d67ca157ccb8
KozichKonstantin <humanstepyt@gmail.com>
2023-08-25 01:37:56 +0300

    25.08.2023 V1.01.2
    working on future features

--- puttingHeroes.js
+++ puttingHeroes.js
@@ -4,6 +4,9 @@ import { savedHeroesBase } from './data.js';
 let test = localStorage.getItem("savedClass");
 document.getElementsByClassName('savedCardNameText')[0].textContent = localStorage.getItem("savedClass")
 document.getElementsByClassName('savedCardPictureImg')[0].src = localStorage.getItem("image") + '.jpg'
+for (let i=0; i < 6; i++){
+    document.getElementsByClassName('spec')[i].textContent = localStorage.getItem(`stats${i}`)
+}
 // function ready() {
 // }
 // document.addEventListener("DOMContentLoaded", ready());
