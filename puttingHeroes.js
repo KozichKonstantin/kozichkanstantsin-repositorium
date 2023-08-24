0c7d0c9216f56974354f696c84af91077d16e98d
sedrex <sedrex353@gmail.com>
2023-08-24 23:29:34 +0300

    1

--- puttingHeroes.js
+++ puttingHeroes.js
@@ -2,6 +2,9 @@ import {data} from './data.js';
 import { savedHeroesBase } from './data.js';
 
 let test = localStorage.getItem("savedClass");
+
+document.getElementsByClassName('savedCardNameText')[0].textContent = localStorage.getItem("savedClass");
+
 // function ready() {
 // }
 // document.addEventListener("DOMContentLoaded", ready());
