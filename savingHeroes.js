32e111bde55f649bb7ca2a6fa376a98277fba6ca
KozichKonstantin <humanstepyt@gmail.com>
2023-08-24 23:03:02 +0300

    24.08.2023 V1.01.2
    mysql

--- savingHeroes.js
+++ savingHeroes.js
@@ -2,7 +2,7 @@ import {data} from './data.js'
 import {savedHeroesBase} from './data.js'
 
 let savingHeroesButtons = document.getElementsByClassName('saveHero');
-for(let i=0; i< data.length; i++){
+for(let i=0; i< savingHeroesButtons.length; i++){
     savingHeroesButtons[i].addEventListener('click', function() {
         const currentHeroButton = savingHeroesButtons[i];
         let savedClass = currentHeroButton.className.toString().slice(9, (currentHeroButton.length))
