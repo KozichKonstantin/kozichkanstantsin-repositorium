d3b3614ea0efaf0b8ba557afd4c972285dbad4aa
sedrex <sedrex353@gmail.com>
2023-08-24 23:02:25 +0300

    24.08.2023 v1.01.2
    localstorage

--- savingHeroes.js
+++ savingHeroes.js
@@ -1,23 +1,33 @@
 import {data} from './data.js'
 import {savedHeroesBase} from './data.js'
 
+
+
+
 let savingHeroesButtons = document.getElementsByClassName('saveHero');
 for(let i=0; i< data.length; i++){
+    // localStorage.clear();
     savingHeroesButtons[i].addEventListener('click', function() {
         const currentHeroButton = savingHeroesButtons[i];
         let savedClass = currentHeroButton.className.toString().slice(9, (currentHeroButton.length))
         if(savedClass == data[i]['name']){
             
             savedHeroesBase[0]['class'] = savedClass;
+
+            localStorage.setItem('savedClass',`${savedHeroesBase[0]['class']}`)
+
             savedHeroesBase[0]['image'] = data[i]['img'] + '/' + data[i]['currentPicture']
+            localStorage.setItem('image',`${savedHeroesBase[0]['image']}`)
+
             let stats = document.getElementsByClassName(('quantity ' + savedClass + 'Spec'));
             for(let j=0; j < 6; j++){
-                savedHeroesBase[1][j] = stats[j].value
+                localStorage.setItem(`stats${j}`,`${stats[j].value}`)
+
             }
             console.log(savedHeroesBase);
             console.log(stats);
             document.getElementsByClassName('savedCardNameText').textContent =`${savedHeroesBase [0]['class']}`
-            console.log(document.getElementsByClassName('savedCardName').textContent, 'suka')
+            console.log(document.getElementsByClassName('savedCardNameText').textContent, 'suka')
             
         }
         // localStorage.setItem("savedCard",JSON.stringify(savedHeroesBase))
