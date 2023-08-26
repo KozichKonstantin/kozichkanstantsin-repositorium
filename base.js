8ad505fac88a7b8e956580c9504b6aebb7c5a16d
KozichKonstantin <humanstepyt@gmail.com>
2023-08-26 20:04:26 +0300

    26.08.2023 V1.01.2
    added sql config

--- base.js
+++ base.js
@@ -1,51 +1,17 @@
-const base = [
-    {
-      "name": "barbarian",
-      "img": "data/grid_cards/barbarian",
-      "count": "5",
-      "currentPicture": 1
-    },
-    {
-      "name": "bard",
-      "img": "data/grid_cards/bard",
-      "count": "4",
-      "currentPicture": 1
-    },
-    {
-      "name": "druid",
-      "img": "data/grid_cards/druid",
-      "count": "5",
-      "currentPicture": 1
-    },
-    {
-      "name": "monach",
-      "img": "data/grid_cards/monach",
-      "count": "5",
-      "currentPicture": 1
-    },
-    {
-      "name": "paladin",
-      "img": "data/grid_cards/paladin",
-      "count": "6",
-      "currentPicture": 1
-    },
-    {
-      "name": "spy",
-      "img": "data/grid_cards/spy",
-      "count": "5",
-      "currentPicture": 1
-    },
-    {
-      "name": "witch",
-      "img": "data/grid_cards/witch",
-      "count": "5",
-      "currentPicture": 1
-    },
-    {
-      "name": "wizard",
-      "img": "data/grid_cards/wizard",
-      "count": "6",
-      "currentPicture": 1
-    }
-  ]
-
+const mysql = require('mysql');
+// const { config } = require('process');
+// konfig
+const conn = mysql.createConnection( config: {
+  host : "127.0.0.1::3306",
+  user : "admin",
+  database : "mastersbookdb",
+  password : "Kostia2002"
+});
+conn.connect( options: err => {
+  if(err){
+    console.log('error');
+  }
+  else {
+    console.log('DB -- Ok');
+  }
+});
