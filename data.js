bb80d5f3fdb279e092b9416a39e9869b70f26bd4
KozichKonstantin <humanstepyt@gmail.com>
2023-08-27 01:49:43 +0300

    27.08.2023 V1.01.2
    slider edited

--- data.js
+++ data.js
@@ -2,49 +2,49 @@ export const data =[
   {
       "name": "barbarian",
       "img": "data/grid_cards/barbarian",
-      "count": "5",
+      "count": 5,
       "currentPicture": 1
     },
     {
       "name": "bard",
       "img": "data/grid_cards/bard",
-      "count": "4",
+      "count": 4,
       "currentPicture": 1
     },
     {
       "name": "druid",
       "img": "data/grid_cards/druid",
-      "count": "5",
+      "count": 5,
       "currentPicture": 1
     },
     {
       "name": "monach",
       "img": "data/grid_cards/monach",
-      "count": "5",
+      "count": 5,
       "currentPicture": 1
     },
     {
       "name": "paladin",
       "img": "data/grid_cards/paladin",
-      "count": "6",
+      "count": 6,
       "currentPicture": 1
     },
     {
       "name": "spy",
       "img": "data/grid_cards/spy",
-      "count": "5",
+      "count": 5,
       "currentPicture": 1
     },
     {
       "name": "witch",
       "img": "data/grid_cards/witch",
-      "count": "5",
+      "count": 5,
       "currentPicture": 1
     },
     {
       "name": "wizard",
       "img": "data/grid_cards/wizard",
-      "count": "6",
+      "count": 6,
       "currentPicture": 1
     }
   ]
