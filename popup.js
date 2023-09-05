c8e82ddd56c09c1ac3791abe69be6ac8090f1348
SedreX <sedrex353@gmail.com>
2023-09-05 18:30:31 +0300

    05,09,23 v1.01.2

--- popup.js
+++ popup.js
@@ -5,13 +5,37 @@ for(let i =0; i<btn_activ.length; i++){
     btn_activ[i].addEventListener('click', function() {
         const number = btn_activ[i].parentNode.parentNode.className.toString().slice(10 , btn_activ[i].length);
         const card =  JSON.parse(localStorage.getItem(`card${number}`));
-        const gg = document.querySelector(".classnameInput"); //dodelat jsonparse
+        const gg = document.querySelector(".classnameInput");
         gg.value = card[2]["class"];
         for(let j=0; j<6; j++){
             document.getElementsByClassName('plusMinus')[j].innerHTML = card[0].valued[j];
             document.getElementsByClassName('stats')[j].innerHTML = Math.floor((card[0].valued[j]-10) /2);
+            document.getElementsByClassName('stats_fix')[j].innerHTML = Math.floor((card[0].valued[j]-10) /2);
         }
-
+        let change = document.getElementsByClassName('Str');
+        for(let k=0; k<change.length; k++){
+            change[k].innerHTML = card[0].valued[0]  ;
+        }
+        change = document.getElementsByClassName('Dex');
+        for(let k=0; k<change.length; k++){
+            change[k].innerHTML = card[0].valued[1]  ;
+        }
+        change = document.getElementsByClassName('Con');
+        for(let k=0; k<change.length; k++){
+            change[k].innerHTML = card[0].valued[2]  ;
+        }
+        change = document.getElementsByClassName('Int');
+        for(let k=0; k<change.length; k++){
+            change[k].innerHTML = card[0].valued[3]  ;
+        }
+        change = document.getElementsByClassName('Wis');
+        for(let k=0; k<change.length; k++){
+            change[k].innerHTML = card[0].valued[4]  ;
+        }
+        change = document.getElementsByClassName('Cha');
+        for(let k=0; k<change.length; k++){
+            change[k].innerHTML = card[0].valued[5]  ;
+        }
 
 
         document.querySelector(".none").classList.add("background")
@@ -51,47 +75,8 @@ function addHover() {
 
 let nums = 0;
 const input_check = document.querySelectorAll(".input_chek")
-// for(let i =0; i<input_check.length;i++){
-// input_check[i].addEventListener('click', function() {
-    // if(input_check[i].checked == true){
-    //    nums++
-    //    console.log(nums)
-    // }
-    // else{   
-    //     nums--
-    //     console.log(nums)
-    // }
-//     var checkboxes = document.querySelectorAll('.input_chek:checked');
-//     for (var input of checkboxes) {
-//         console.log("hell")
-//     }
-//   })
-// }
+const stats_fix = document.querySelectorAll(".stats_fix")
 
-// for(let i =0; i<input_check.length; i++){
-//     input_check[i].addEventListener('change', disable);
-// }
-// function disable(){
-//     for(let i =0; i<input_check.length; i++){
-//         input_check[i].addEventListener('change', disable_all);
-//     }
-// }
-
-// function disable_all(){
-//     for(let i=0; i<input_check.length; i++){
-//         input_check[i].disabled = input_check[i].name !=this.name;
-//         for(let j =0; j<input_check.length; j++){
-//             input_check[j].addEventListener('change', anable_all);
-//         }
-        
-//     }
-//     console.log ('haha')
-// }
-// function anable_all(){
-//     for(let i=0; i<input_check.length; i++){
-//         input_check[i].disabled = false;
-//     }
-// }
 localStorage.setItem('counter', 0) ;
 for(let i =0; i<input_check.length; i++){
     input_check[i].addEventListener('click', ()=>{
@@ -102,25 +87,41 @@ for(let i =0; i<input_check.length; i++){
                 }
             }
             console.log('opa')
-        }else{
-      /////// с этим кодом потом будут проблемы в сохранении листов из-за 
-      /////// обнуления счетчика, но без обнуления при перезагрузке страницы 
-      /////// все полетит по хуям      
-        }
-        if(input_check[i].checked == true){
+        } if(input_check[i].checked == true){
             localStorage.setItem('counter', localStorage.getItem('counter')-(-1)) ;
+            stats_fix[i].textContent = stats_fix[i].textContent - (-3);
         }else{
+            stats_fix[i].textContent = stats_fix[i].textContent -3;
             localStorage.setItem('counter', localStorage.getItem('counter')-1)
             for(let j =0; j < input_check.length; j++){
                 if(input_check[j].checked == false){
                     input_check[j].disabled = false;
+                    
                 }
             }
         }
         
     })
-        
-    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let coins = document.querySelectorAll('.coin');
 for (let i=0; i< coins.length; i++){
     coins[0].style.backgroundColor = '#b87333';
@@ -129,3 +130,4 @@ for (let i=0; i< coins.length; i++){
     coins[3].style.backgroundColor = 'gold';
     coins[4].style.backgroundColor = '#e5e4e2';
 }
+
